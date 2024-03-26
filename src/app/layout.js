0b8b8f9ed5b55e4d8b968bc64549cf167bcca94d
rehatpsingh <rehatpsingh@gmail.com>
2024-03-26 19:39:06 -0400

    updated bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,8 +5,8 @@ import TransitionProvider from "@/components/transitionProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Lama Dev Portfolio App",
-  description: "The best animated portfolio page",
+  title: "Rehat portfolio website App",
+  description: "Full Stack Developer",
 };
 
 export default function RootLayout({ children }) {
--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -8,7 +8,7 @@ const items = [
   {
     id: 1,
     color: "from-red-300 to-blue-300",
-    title: "TechInnovate Solutions/ HTML CSS",
+    title: "TechInnovate Solutions",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
     link: "https://rehatpsingh.github.io/IT-Solutions-Providing-Website---HTML-CSS/",
@@ -27,7 +27,7 @@ const items = [
     title: "Vanilla Book App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    link: "https://lama.dev",
+    link: "",
   },
   {
     id: 4,
@@ -35,7 +35,7 @@ const items = [
     title: "Spotify Music App",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
     img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    link: "https://lama.dev",
+    link: "",
   },
 ];
 
